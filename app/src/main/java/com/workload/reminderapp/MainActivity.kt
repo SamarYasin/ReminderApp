e6953315d64d25bcf90649f1cbe364881286b58a
samar.yasin <Samar.M.Yasin@gmail.com>
2025-02-11 16:05:16 +0500

    Minor Changes

--- app/src/main/java/com/workload/reminderapp/MainActivity.kt
+++ app/src/main/java/com/workload/reminderapp/MainActivity.kt
@@ -3,8 +3,6 @@ package com.workload.reminderapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
 import com.workload.style.ReminderAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -16,10 +14,4 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-}
-
-@Preview(apiLevel = 34)
-@Composable
-fun GreetingPreview() {
-
 }
